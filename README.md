fe8bac8411f34b4d215af5cf5e1cce4b7e162aa0
chenzhangjin <467193599@qq.com>
2024-04-24 15:44:55 +0800

    feat: modify README.md

--- README.md
+++ README.md
@@ -11,29 +11,33 @@ and the Flutter guide for
 [developing packages and plugins](https://flutter.dev/developing-packages).
 -->
 
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
-
-## Features
-
-TODO: List what your package can do. Maybe include images, gifs, or videos.
-
-## Getting started
-
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
-
 ## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
 
 ```dart
-const like = 'sample';
+CountDownWidget(
+  countDownTime: timeStamp,
+  builder: (context, day, hour, minute, second) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 168.0),
+      height: 18.0,
+      alignment: Alignment.center,
+      margin: const EdgeInsets.only(top: 3.5),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: "$day",
+              style: const TextStyle(fontSize: 13.0, color: Colors.black, fontWeight: FontWeight.w500),
+            ),
+            ...
+          ],
+        ),
+      ),
+    );
+  },
+)
 ```
 
-## Additional information
-
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+## More
+For more question, you can email me: chenzhangjin@outlook.com
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int timeStamp = 1840060800000;
   @override
   void initState() {
     super.initState();
@@ -26,54 +27,60 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Flutter Countdown Example'),
         ),
         body: Center(
-          child: CountDownWidget(
-            countDownTime: 40404039934,
-            builder: (context, day, hour, minute, second) {
-              return Container(
-                constraints: const BoxConstraints(maxWidth: 168.0),
-                height: 18.0,
-                alignment: Alignment.bottomLeft,
-                margin: const EdgeInsets.only(top: 3.5),
-                child: RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "$day",
-                        style: const TextStyle(fontSize: 13.0, color: Colors.black, fontWeight: FontWeight.w500),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("${DateTime.fromMillisecondsSinceEpoch(timeStamp)}"),
+              CountDownWidget(
+                countDownTime: timeStamp,
+                builder: (context, day, hour, minute, second) {
+                  return Container(
+                    constraints: const BoxConstraints(maxWidth: 168.0),
+                    height: 18.0,
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.only(top: 3.5),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "$day",
+                            style: const TextStyle(fontSize: 13.0, color: Colors.black, fontWeight: FontWeight.w500),
+                          ),
+                          const TextSpan(
+                            text: "天 ",
+                            style: TextStyle(fontSize: 10.0, color: Color(0xffa29bb0)),
+                          ),
+                          TextSpan(
+                            text: "$hour",
+                            style: const TextStyle(fontSize: 13.0, color: Colors.black, fontWeight: FontWeight.w500),
+                          ),
+                          const TextSpan(
+                            text: "时 ",
+                            style: TextStyle(fontSize: 10.0, color: Color(0xffa29bb0)),
+                          ),
+                          TextSpan(
+                            text: "$minute",
+                            style: const TextStyle(fontSize: 13.0, color: Colors.black, fontWeight: FontWeight.w500),
+                          ),
+                          const TextSpan(
+                            text: "分 ",
+                            style: TextStyle(fontSize: 10.0, color: Color(0xffa29bb0)),
+                          ),
+                          TextSpan(
+                            text: "$second",
+                            style: const TextStyle(fontSize: 13.0, color: Colors.black, fontWeight: FontWeight.w500),
+                          ),
+                          const TextSpan(
+                            text: "秒 ",
+                            style: TextStyle(fontSize: 10.0, color: Color(0xffa29bb0)),
+                          ),
+                        ],
                       ),
-                      const TextSpan(
-                        text: "天 ",
-                        style: TextStyle(fontSize: 10.0, color: Color(0xffa29bb0)),
-                      ),
-                      TextSpan(
-                        text: "$hour",
-                        style: const TextStyle(fontSize: 13.0, color: Colors.black, fontWeight: FontWeight.w500),
-                      ),
-                      const TextSpan(
-                        text: "时 ",
-                        style: TextStyle(fontSize: 10.0, color: Color(0xffa29bb0)),
-                      ),
-                      TextSpan(
-                        text: "$minute",
-                        style: const TextStyle(fontSize: 13.0, color: Colors.black, fontWeight: FontWeight.w500),
-                      ),
-                      const TextSpan(
-                        text: "分 ",
-                        style: TextStyle(fontSize: 10.0, color: Color(0xffa29bb0)),
-                      ),
-                      TextSpan(
-                        text: "$second",
-                        style: const TextStyle(fontSize: 13.0, color: Colors.black, fontWeight: FontWeight.w500),
-                      ),
-                      const TextSpan(
-                        text: "秒 ",
-                        style: TextStyle(fontSize: 10.0, color: Color(0xffa29bb0)),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
