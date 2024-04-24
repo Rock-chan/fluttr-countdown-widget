481fa3fc0d360c77f69df409ef938555bba33157
chenzhangjin <467193599@qq.com>
2024-04-24 15:48:34 +0800

    feat: modify project name

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:countdown_widget/countdown_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_countdown_widget/countdown_widget.dart';
 
 void main() {
   runApp(const MyApp());
